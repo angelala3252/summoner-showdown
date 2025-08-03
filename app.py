1dc752aff0e35483cf1ea6fe0c8288582bedd325
Angela Zhuo <angelazhuo@hotmail.com>
2025-08-03 17:51:42 -0400

    Set up server

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from main import get_expected_win_rate, get_puuid, get_player_ranking
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/prediction', methods=['POST'])
+def receive_teams():
+    data = request.get_json()
+    print(data)
+    # return jsonify(get_prediction(data)), 200
+    return jsonify({"team1": 0.63, "team2": 0.37}), 200
+
+def get_prediction(data):
+    team1 = data.get("team1")
+    team2 = data.get("team2")
+    if not team1 or not team2:
+        return {"error": "Both teams must be provided."}, 400
+
+    team1_puuid, team2_puuid = get_puuid(team1, team2)
+    t1_expected_win, t2_expected_win = get_expected_win_rate(team1_puuid, team2_puuid)
+    return {"team1": t1_expected_win, "team2": t2_expected_win}
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- frontend/src/pages/home/home-page.tsx
+++ frontend/src/pages/home/home-page.tsx
@@ -14,7 +14,7 @@ export function HomePage() {
             team2: team2Players,
         };
         try {
-            const response = await fetch('/backend-endpoint', {
+            const response = await fetch('http://127.0.0.1:5000/prediction', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(payload),
