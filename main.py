5ad7d9a423e886336a4d112279121a68a33f0e7e
kebina <k396li@uwaterloo.ca>
2025-08-03 18:10:43 -0400

    quick maths

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 import json
+import time
 
-api_key = "RGAPI-f563aeff-cbe8-434a-a816-89801df6452b"
+api_key = "RGAPI-367e41a4-7e32-4a31-8581-1fb9b96094b4"
+
 team1 = [
     {"gameName": "cant type", "tagLine": "1998"},
     {"gameName": "C9 Loki", "tagLine": "kr3"},
@@ -42,31 +44,6 @@ def get_puuid(
     return team1_puuid, team2_puuid
 
 
-# def get_game_ids(
-#     team1_puuid,
-#     team2_puuid
-#     ):
-#     team1_game_ids = {}
-#     team2_game_ids = {}
-#     num_games = 10 # default number of historical games to fetch
-    
-#     for puuid in team1_puuid:
-#         player_game_ids = fetch_something(
-#             f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={num_games}",
-#             headers={"X-Riot-Token": api_key}
-#         )
-#         team1_game_ids[puuid] = player_game_ids
-
-#     for puuid in team2_puuid:
-#         player_game_ids = fetch_something(
-#             f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={num_games}",
-#             headers={"X-Riot-Token": api_key}
-#         )
-#         team2_game_ids[puuid] = player_game_ids
-
-#     return team1_game_ids, team2_game_ids
-
-
 def get_player_info(puuid):
     player_info = {}
     fields = ["tier", "rank", "leaguePoints", "wins", "losses"]
@@ -86,11 +63,12 @@ def get_num_rank(
         "BRONZE": 400,
         "SILVER": 800,
         "GOLD": 1200,
-        "PLATINUM": 1600,
-        "DIAMOND": 2000,
-        "MASTER": 2400,
-        "GRANDMASTER": 2600,
-        "CHALLENGER": 3100
+        "EMERALD": 1600,
+        "PLATINUM": 2000,
+        "DIAMOND": 2400,
+        "MASTER": 2500,
+        "GRANDMASTER": 2700,
+        "CHALLENGER": 3200
     }
     rank_values = {
         "IV": 0,
@@ -104,7 +82,7 @@ def get_num_rank(
 
 
 def get_player_ranking(puuid):
-    num_games = 10
+    num_games = 3
     player_info = get_player_info(puuid)
 
     prev_game_ids = fetch_something(
@@ -123,32 +101,121 @@ def get_player_ranking(puuid):
     return rankings[-1]
 
 
-# game info ->
-    # kills
-    # deaths
-    # assists
-    # totalDamageDealt
-    # totalDamageTaken
-    # trueDamageDealt
-    # trueDamageTaken
-    # win   
-
 def update_ranking(
     puuid, 
     rank, 
     game_id):
 
-    participants = fetch_something(
+    game_info = fetch_something(
         f"https://americas.api.riotgames.com/lol/match/v5/matches/{game_id}",
-        headers={"X-Riot-Token": api_key})["metadata"]["participants"]
-    
-    team_rankings = []
-    for participant in participants:
-        rank = get_num_rank(participant)
-        team_rankings.append(rank)
-    
+        headers={"X-Riot-Token": api_key})
 
-    return 0
+    participants = game_info["metadata"]["participants"]
+    
+    team1 = []
+    team2 = []
+
+    for ind in range(len(participants)):
+        team = game_info["info"]["participants"][ind]["teamId"]
+        if team == 100:
+            team1.append(participants[ind])
+        else:
+            team2.append(participants[ind])
+
+    team1_dict = {}
+    team2_dict = {}
+    
+    for player in team1:
+        team1_dict[player] = {}
+        player_info = get_player_info(player)
+
+        games = player_info["wins"] + player_info["losses"] 
+        k_value = 800 / (games +1)
+        team1_dict[player]["K"] = k_value
+
+        damage_dealt = game_info["info"]["participants"][ind]["trueDamageDealt"]
+        team1_dict[player]["DamageDelt"] = damage_dealt
+
+        damage_taken = game_info["info"]["participants"][ind]["trueDamageTaken"]
+        team1_dict[player]["DamageTaken"] = damage_taken
+
+        if player == puuid:
+            ind = team1.index(player)
+            win = game_info["info"]["participants"][ind]["win"]
+            team1_dict[player]["rank"] = rank
+            puuid_team = 1
+        
+        else:
+            num_rank = get_num_rank(player_info["tier"], player_info["rank"], player_info["leaguePoints"])
+            team1_dict[player]["rank"] = num_rank
+
+    for player in team2:
+        team2_dict[player] = {}
+        player_info = get_player_info(player)
+
+        games = player_info["wins"] + player_info["losses"] 
+        k_value = 800 / (games +1)
+        team2_dict[player]["K"] = k_value
+
+        damage_dealt = game_info["info"]["participants"][ind]["trueDamageDealt"]
+        team2_dict[player]["DamageDelt"] = damage_dealt
+
+        damage_taken = game_info["info"]["participants"][ind]["trueDamageTaken"]
+        team2_dict[player]["DamageTaken"] = damage_taken
+
+        if player == puuid:
+            ind = team2.index(player)
+            win = game_info["info"]["participants"][ind]["win"]
+            team2_dict[player]["rank"] = rank
+            puuid_team = 2
+
+        else:
+            num_rank = get_num_rank(player_info["tier"], player_info["rank"], player_info["leaguePoints"])
+            team2_dict[player]["rank"] = num_rank
+    
+    # get team ranks
+    team1_rank = sum(team1_dict[player]["rank"] for player in team1_dict)
+    team2_rank = sum(team2_dict[player]["rank"] for player in team2_dict)
+
+    # get team k values
+    t1_k = sum(team1_dict[player]["K"] for player in team1_dict)
+    t2_k = sum(team2_dict[player]["K"] for player in team2_dict)
+    
+    # get expected win rate
+    t1_win_rate = 1 / (1 + 10 ** ((team2_rank - team1_rank) / 400))
+    t2_win_rate = 1 - t1_win_rate
+
+    # get score and adjust ranking, split adjustment among team
+    if puuid_team == 1:
+        if win:
+            score = 1
+        else:
+            score = 0
+        new_team1_rank = team1_rank + t1_k * (score - t1_win_rate)
+        if win:
+            total_dmg = sum(team1_dict[player]["DamageDelt"] for player in team1_dict)
+            percentage = team1_dict[puuid]["DamageDelt"] / total_dmg
+            new_rank = new_team1_rank * percentage
+        else:
+            total_dmg = sum(team1_dict[player]["DamageTaken"] for player in team1_dict)
+            percentage = team1_dict[puuid]["DamageTaken"] / total_dmg
+            new_rank = new_team1_rank * percentage
+    else:
+        if win:
+            score = 1
+        else:
+            score = 0
+        new_team2_rank = team2_rank + t2_k * (score - t2_win_rate)
+        if win: 
+            total_dmg = sum(team2_dict[player]["DamageDelt"] for player in team2_dict)
+            percentage = team2_dict[puuid]["DamageDelt"] / total_dmg
+            new_rank = new_team2_rank * percentage
+        else:   
+            total_dmg = sum(team2_dict[player]["DamageTaken"] for player in team2_dict)
+            percentage = team2_dict[puuid]["DamageTaken"] / total_dmg
+            new_rank = new_team2_rank * percentage
+
+    return new_rank
 
 
 def get_expected_win_rate(
@@ -174,6 +241,7 @@ def get_expected_win_rate(
 
 
 def fetch_something(url, params=None, data=None, headers=None, cookies=None, auth=None, timeout=None, allow_redirects=True):
+    time.sleep(1)  # To avoid hitting rate limits
     response = requests.get(
         url,
         params=params,
